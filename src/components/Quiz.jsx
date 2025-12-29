129c45b55e94ae13b9088f805944ae203edca28a
mostafa93sh <mostafa.khalel.93@gmail.com>
2025-12-30 01:40:56 +0200

    adding logic to Quiz.jsx | shuffeled answers | quiz finished banner

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,21 +1,36 @@
 import { useState } from "react";
 import Question from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const currentQuestionIndex = userAnswers.length;
   const currentQuestion = Question[currentQuestionIndex];
+  const quizIsFinished = userAnswers.length === Question.length;
 
   function handleAnswerSelection(answer) {
     setUserAnswers((prevUserAnswers) => [...prevUserAnswers, answer]);
   }
+
+  if (quizIsFinished) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="quiz-finished" />
+        <h2>Quiz Finished!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...currentQuestion.answers].sort(
+    () => Math.random() - 0.5
+  );
 
   return (
     <div id="quiz">
       <div id="question">
         <h2>{currentQuestion?.text}</h2>
         <ul id="answers">
-          {currentQuestion?.answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleAnswerSelection(answer)}>
                 {answer}
